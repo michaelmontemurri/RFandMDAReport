da3cc2efae4a1db70c8b5b62a59f60c3282a6aba
Michael Montemurri <michaelmontemurri@mail.mcgill.ca>
2024-11-15 13:04:37 -0500

    plotting mda_simulation

--- mda_simulation.py
+++ mda_simulation.py
@@ -16,6 +16,7 @@ def generate_data(n_samples, n_features, c, tau_0):
     I = np.eye(n_features)
     ones = np.ones(n_features)
     C = (1-c)*I + c*np.outer(ones, ones)
+
     #make tau a column vector of tau_0 of lengeth p
     tau = [tau_0]*n_features
 
@@ -38,31 +39,27 @@ def generate_data(n_samples, n_features, c, tau_0):
 
 #MDA result from Gregorutti (2016)
 def mda_star(X, Y, tau_0, c, p):
-    print(tau_0)
+ 
     theoretical_mda = 2 * (tau_0 / (1 - c + p * c)) ** 2 
-    print(theoretical_mda)
     return theoretical_mda
 
 #lets do a littel simulation to see if the result holds when we compare to the sklearn permutation importance
-changes = []
+
 n_samples = 1000
-n_features = 2
-c = 0.01
-tau_0 = 0.5
-X,Y = generate_data(n_samples, n_features, c, tau_0)
-print(X,Y)
+p = 5
+c = 0.95
+tau_0 = 0.9
+X,Y = generate_data(n_samples, p, c, tau_0)
 
 #use generated data to fit a random forest model
-rf = RandomForestRegressor(n_estimators=1000, max_depth=None, max_features='sqrt', n_jobs=-1, random_state=42)
+rf = RandomForestRegressor(n_estimators=1000, max_depth=None, max_features='sqrt', n_jobs=-1)
 rf.fit(X, Y)
 perm_importance = permutation_importance(rf, X, Y, n_repeats=20, random_state=42)
 importance = perm_importance.importances_mean
 
-
 #now we want to comapre the result from the MDA formula to the permutation importance
-
 #calculate MDA^*(X^(j)) for all j
-theoretical_mda = mda_star(X, Y, tau_0, c, n_features)
+theoretical_mda = mda_star(X, Y, tau_0, c, p)
 
 print('theoretical mda', theoretical_mda)
 print('sklearn perm. importatnce', importance)
